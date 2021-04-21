4f6045161b92d29bd80e4eb3ab4cfe765b146917
principalkelvoe <you@example.com>
2021-04-21 22:59:52 +0300

    add dashboard tp router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/login.vue'
+import Dashboard from '../views/dashboard/Dashboard.vue'
 
 const routes = [
   {
@@ -26,6 +27,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   }
 ]
 
