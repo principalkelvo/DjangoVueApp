beea1d32bdbc241b344e8c098594076e20f4673a
principalkelvoe <you@example.com>
2021-04-22 21:16:28 +0300

    importing axios and creating beforeCreate()

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,30 @@
 
 
 <script>
+//import axios to run before create function
+import axios from 'axios'
 import Navbar from '../src/components/layout/Navbar'
 
 export default {
   name:'App',
   components:{
     Navbar
+  },
+
+  //this is one of vue lifecycle, runs store when we open our website
+  beforeCreate(){
+    this.$store.commit('initializeStore')
+
+    //check if token exists
+    if (this.$store.state.token){
+    axios.defaults.headers.common['Authorization']="Token" + this.$store.state.token
+    }
+    //if you are not authenticated
+  else{
+    axios.defaults.headers.common['Authorization']= ""
   }
+  }
+
 }
 </script>
 <style lang="scss">
