0f19c0a4b226edf48d012f8998796af9dabac28b
principalkelvoe <you@example.com>
2021-04-24 06:17:07 +0300

    change ' ' to ` ` and ( ) to { }

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -90,7 +90,7 @@ export default {
                 .catch(error=>{
                     if(error.response){
                         for(const property in error.response.data){
-                            this.errors.push('${property}: $(error.response.data[property])')
+                            this.errors.push(`${property}: ${error.response.data[property]}`)
                         }
                     }
                     else if(error.message){
