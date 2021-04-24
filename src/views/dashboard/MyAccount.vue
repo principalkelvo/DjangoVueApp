d70c04ce8ebeb978d0830ab0cad31d2257e106a7
principalkelvoe <you@example.com>
2021-04-24 20:43:15 +0300

    created log out button

--- src/views/dashboard/MyAccount.vue
+++ src/views/dashboard/MyAccount.vue
@@ -4,11 +4,28 @@
             <div class="column is-12">
                 <h1 class="title">MyAccount</h1>
             </div>
+            <button @click="logout()" class="button is-danger">Log Out</button>
         </div>
     </div>
 </template>        
 <script>
+import axios from 'axios'
 export default {
-    name: 'MyAccount'
+    name: 'MyAccount',
+    methods:{
+        async logout(){
+            await axios
+                .post('/api/v1/token/logout/')
+                .then(response=>{
+                    console.log('logged out')
+                })
+                .catch(error=>{
+                    console.log(JSON.stringify(error))
+                })
+                axios.defaults.headers.common['Authorization']=''
+                localStorage.removeItem('token')
+                this.$router.push('/')
+        }
+    }
 }
 </script>
