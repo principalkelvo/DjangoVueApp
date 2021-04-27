7d0d1ad0c4cb73e984738b109946f5303e1203a5
principalkelvoe <you@example.com>
2021-04-27 21:58:19 +0300

    created addLead page

---
+++ src/views/dashboard/AddLead.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">AddLead/h1>
+            </div>
+        </div>
+    </div>
+</template>        
+<script>
+export default {
+    name: 'AddLead'
+}
+</script>
