29ff08367cc86d8fdebec2eb16e21ac5bf928ee1
principalkelvoe <you@example.com>
2021-04-27 21:57:13 +0300

    lead table and axios. Got error on refreshing page

--- src/views/dashboard/Leads.vue
+++ src/views/dashboard/Leads.vue
@@ -4,6 +4,27 @@
             <div class="column is-12">
                 <h1 class="title">Leads</h1>
             </div>
+
+            <div class="column is-12 ">
+                <table class="table is-fullwidth">
+                    <thead>
+                        <tr>
+                            <th>Company</th>
+                            <th>Contact Person</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr v-for="lead in leads" v-bind:key="lead.id">
+                            <td>{{lead.company}}</td>
+                            <td>{{lead.contact_person}}</td>
+                            <td>{{lead.status}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     </div>
 </template>        
@@ -15,7 +36,7 @@ export default {
     name: 'Leads',
     data(){
         return{
-            Leads:[]
+            leads:[]
         }
     },
     mounted(){
@@ -26,6 +47,15 @@ export default {
             this.$store.commit('setIsLoading', true)
 
             axios
+            .get('/api/v1/leads/')
+            .then(response=>{
+                this.leads= response.data
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+
+            this.$store.commit('setIsLoading', false)
         }
     }
 }
