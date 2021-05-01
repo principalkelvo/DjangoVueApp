8258c766bc4c2d3fd419c43eb7f27f07e7014f5e
principalkelvoe <you@example.com>
2021-05-02 01:06:17 +0300

    created edit lead  added form and script

---
+++ src/views/dashboard/EditLead.vue
@@ -0,0 +1,132 @@
+<template>
+     <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">Edit {{lead.company}}</h1>
+            </div>
+
+            <div class="column is-12">
+                <form @submit.prevent="submitForm">
+                    <!--company field-->
+                    <div class="field">
+                        <label>Company</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="lead.company">
+
+                        </div>
+                    </div>
+                    <!--contact person field-->
+                    <div class="field">
+                        <label>Contact person</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="lead.contact_person">
+                        </div>
+                    </div>
+                    <!--email field-->
+                    <div class="field">
+                        <label>Email</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="lead.email">
+                        </div>
+                    </div>
+                    <!--phone field-->
+                    <div class="field">
+                        <label>Phone</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="lead.phone">
+                        </div>
+                    </div>
+                    <!--website field-->
+                    <div class="field">
+                        <label>Website</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="lead.website">
+                        </div>
+                    </div>
+                    <!--estimated value field-->
+                    <div class="field">
+                        <label>Estimated value</label>
+                        <div class="control">
+                            <input type="number" class="input" v-model="lead.estimated_value">
+                        </div>
+                    </div>
+                    <!--confidence field-->
+                    <div class="field">
+                        <label>Confidence</label>
+                        <div class="control">
+                            <input type="number" class="input" v-model="lead.confidence">
+                        </div>
+                    </div>
+                    <!--status field with select item-->
+                    <div class="field">
+                        <label>Status</label>
+                        <div class="control">
+                            <div class="select">
+                                <select v-model="lead.status">
+                                    <option value="new">New</option>
+                                    <option value="contacted">Contacted</option>
+                                    <option value="inprogress">In Progress</option>
+                                    <option value="lost">Lost</option>
+                                    <option value="won">Won</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Priority field with select item-->
+                    <div class="field">
+                        <label>Priority</label>
+                        <div class="control">
+                            <div class="select">
+                            <select v-model="lead.priority">
+                                <option value="low">Low</option>
+                                <option value="medium">Medium</option>
+                                <option value="high">High</option>
+                            </select>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Update button-->
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-success">Update</button>
+                        </div>
+                    </div>
+
+
+                </form>
+            </div>  
+
+        </div>
+     </div>
+</template>
+<script>
+export default {
+    name: 'EditLead',
+    data(){
+        return{
+            lead: {}
+        }
+    },
+     mounted(){
+        this.getLead()
+    },
+    methods:{
+        async getLead(){
+            this.$store.commit('setIsLoading', true)
+
+            const leadID= this.$route.params.id
+            axios
+                .get(`/api/v1/leads/${leadID}`)// used backslash `` not quotation
+                .then(response=>{
+                    this.lead= response.data
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+
+            this.$store.commit('setIsLoading', false)
+        }
+    }
+    
+}
+</script>
