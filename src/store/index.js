cba03ff3e8134fc4a6ed6e6144ab023c883487d3
principalkelvoe <you@example.com>
2021-05-02 22:40:30 +0300

    stored user info in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -17,10 +17,15 @@ export default createStore({
         //if yes
         state.token = localStorage.getItem('token')
         state.isAuthenticated = true
+        state.user.username= localStorage.getItem('username')
+        state.user.id= localStorage.getItem('userid')
+
       }//if not logged in
       else{
         state.token= ''
         state.isAuthenticated = false
+        state.user.id= 0
+        state.user.username= ''
       }
     },
     //method for change of loading status 
@@ -36,6 +41,9 @@ export default createStore({
     removeToken(state){
       state.token= ''
       state.isAuthenticated= false
+    },
+    setUser(state,user){
+      state.user= user
     }
   },
   actions: {
