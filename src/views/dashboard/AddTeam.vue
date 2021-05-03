655bbe2f6bf44f70449b58750bb1652c2a20a009
principalkelvoe <you@example.com>
2021-05-03 23:37:42 +0300

    added form and method submitForm()

--- src/views/dashboard/AddTeam.vue
+++ src/views/dashboard/AddTeam.vue
@@ -4,12 +4,64 @@
             <div class="column is-12">
                 <h1 class="title">Add team</h1>
             </div>
+            <div class="column is-12">
+                <form @submit.prevent="submitForm">
+                   
+                    <!--contact person field-->
+                    <div class="field">
+                        <label>Team name</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="name">
+                        </div>
+                    </div>
+                    
+                    <!--submit button-->
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-success">Submit</button>
+                        </div>
+                    </div>
+
+
+                </form>
+
+            </div>
         </div>
     </div>
 
 </template>
 <script>
+import axios from 'axios'
 export default {
-    name: 'AddTeam'
+    name: 'AddTeam',
+    methods:{
+        async submitForm(){
+            this.$store.commit('setIsLoading', true)
+            const team= {
+                name: this.name
+            }
+
+            await axios
+                .post('/api/v1/teams/', team)
+                .then(response=>{
+                    toast({
+                            message:'The team was added',
+                            type:'is-success',
+                            dismissible: true,
+                            pauseOnHover: true,
+                            duration: 2000,
+                            position: 'bottom-right',
+                        })
+                    
+                    this.$store.commit('setTeam',{'id': response.data.id,'name':this.name })
+
+                    this.$router.push('/dashboard')
+                })
+                
+                .catch(error=>{
+                    console.log(error)
+                })
+        }
+    }
 }
 </script>
