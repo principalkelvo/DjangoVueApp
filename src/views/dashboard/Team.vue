7d65b2871284c6ac5fd4fb5bf4ce033d80a0920d
principalkelvoe <you@example.com>
2021-05-08 19:55:54 +0300

    created team page

---
+++ src/views/dashboard/Team.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">{{team.name}}</h1>
+                <div class="column is-12">
+                    <h2 class="subtitle">Members</h2>
+                    <!--create table to run data-->
+                    <table class="table is-fullwidth">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                            v-for="member in team.members"
+                            v-bind:key="member.id">
+                            <td>{{member.username}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script> 
+import axios from 'axios'
+export default {
+    name: 'Team',
+    data(){
+        return{
+            team:{
+                members:[]
+            }
+        }
+    },
+    mounted(){
+        this.getTeam()
+    },
+    methods:{
+        async getTeam(){
+            this.$store.commit('setIsLoading', true)
+
+            await axios
+                .get('/api/v1/teams/get_my_team/')
+                .then(response=>{
+                    this.team= response.data
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+
+            this.$store.commit('setIsLoading', false)
+        }
+    }
+}
+</script>
