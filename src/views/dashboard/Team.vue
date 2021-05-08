39a6471a08e2cc3885868904769e42b3d8d694f0
principalkelvoe <you@example.com>
2021-05-08 23:18:41 +0300

    added button in team to route to addMember page

--- src/views/dashboard/Team.vue
+++ src/views/dashboard/Team.vue
@@ -3,6 +3,8 @@
         <div class="columns is-multiline">
             <div class="column is-12">
                 <h1 class="title">{{team.name}}</h1>
+
+                <router-link :to="{'name':'AddMember'}" class="button is-primary">Add member</router-link>
                 <div class="column is-12">
                     <h2 class="subtitle">Members</h2>
                     <!--create table to run data-->
