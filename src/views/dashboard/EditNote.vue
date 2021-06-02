56d45ca9f6901c1e0e9a1692741b77f458cb1adc
principalkelvoe <you@example.com>
2021-06-02 15:52:17 +0300

    edit page

---
+++ src/views/dashboard/EditNote.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">Edit note</h1>
+            </div>
+            <div class="column is-12">
+                <form @submit.prevent="submitForm">
+                    <!--name field-->
+                    <div class="field">
+                        <label>name</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="note.name">
+
+                        </div>
+                    </div>
+                    <!--body field-->
+                    <div class="field">
+                        <label>body</label>
+                        <div class="control">
+                            <textarea class="textarea" v-model="note.body"></textarea>
+                        </div>
+                    </div>
+                    
+                    <!--submit button-->
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-success">Update</button>
+                        </div>
+                    </div>
+
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+</template>        
+<script>
+import axios from 'axios'
+import { toast } from 'bulma-toast'
+
+export default {
+    name: 'EditNote',
+    data(){
+        return{
+            note:{}          
+        }
+    },
+
+    mounted(){
+        this.getNote()
+    },
+
+    methods:{
+        async getNote(){
+            this.$store.commit('setIsLoading', true)
+
+            const noteID= this.$route.params.note_id
+            await axios
+                .get(`/api/v1/notes/${noteID}`)// used backslash `` not quotation
+                .then(response=>{
+                    this.note= response.data
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+
+            this.$store.commit('setIsLoading', false)
+        },
+
+        async submitForm(){
+            
+            this.$store.commit('setIsLoading', true)
+
+            await axios
+                .patch(`/api/v1/notes/${this.note.id}`, this.note)
+                .then(response=>{
+
+                    toast({
+                            message:'The note was updated',
+                            type:'is-success',
+                            dismissible: true,
+                            pauseOnHover: true,
+                            duration: 2000,
+                            position: 'bottom-right',
+                        })
+
+                    this.$router.push({name:'Client',params:{id:this.$route.params.id}})
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+
+                this.$store.commit('setIsLoading',false)
+        }
+    }
+}
+</script>
