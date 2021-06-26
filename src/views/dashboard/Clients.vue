b2bb33aa2b118d0c39506080f2630cffea8af963
principalkelvoe <you@example.com>
2021-06-26 07:53:48 +0300

    added search button to clients working properly

--- src/views/dashboard/Clients.vue
+++ src/views/dashboard/Clients.vue
@@ -5,6 +5,20 @@
                 <h1 class="title">Clients</h1>
 
                 <router-link to="/dashboard/clients/add">Add client</router-link>
+
+                <!--search for leads button-->
+                <hr>
+
+                <form @submit.prevent="getClients">
+                    <div class="field has-addons">
+                        <div class="control">
+                            <input type="text" class="input" v-model="query">
+                        </div>
+                        <div class="control">
+                            <button class="button is-success">Search</button>
+                        </div>
+                    </div>
+                </form>
             </div>
 
             <div class="column is-12 ">
@@ -52,7 +66,8 @@ export default {
             //add next and previous button
             showNextButton:false,
             showPreviousButton:false,
-            currentPage:1
+            currentPage:1,
+            query:''
         }
     },
     mounted(){
@@ -74,7 +89,7 @@ export default {
             this.showPreviousButton=false
 
             await axios
-                .get(`/api/v1/clients/?page=${this.currentPage}`)
+                .get(`/api/v1/clients/?page=${this.currentPage}&search=${this.query}`)
                 .then(response=>{
                     this.clients= response.data.results
 
