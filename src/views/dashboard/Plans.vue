ab6981c6f1a4b9d0e1a7b66d8da8796c5b4d7e8a
principalkelvoe <you@example.com>
2021-07-02 07:48:10 +0300

    created plans page

---
+++ src/views/dashboard/Plans.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">{{plan.name}}</h1>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script> 
+import axios from 'axios'
+export default {
+    name: 'Plans',
+    methods:{
+        
+    }
+}
+</script>
